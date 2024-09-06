8ef952ff42a9871aecfa9a843b7f812f42a61a9a
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-09-06 16:45:20 +0700

    video 28: component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+const MyComponent = () => {
+  return <div>duck dep dzai</div>;
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +19,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
