ae2a82d8124cfd4067936f49d5642e0c6cc6bf2f
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-09-08 17:48:28 +0700

    video 300

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+const MyComponent = () => {
+  return (
+    <>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        duck dep dzai
+      </div>
+      <div>duck dep dzai</div>
+    </>
+  );
+};
+
+export default MyComponent;
