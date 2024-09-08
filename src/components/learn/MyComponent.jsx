23e0c3030eeba98d31af4ead59bd5fd1dcb1a2de
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-09-08 21:11:45 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,18 @@
 import "./style.css";
 const MyComponent = () => {
+  const hoidanit = "eric 1";
+  const duckObject = {
+    name: "Duck",
+    age: 18,
+  };
   return (
     <>
       <div className="child" style={{ borderRadius: "10px" }}>
-        duck dep dzai
+        {hoidanit}&duck dep dzai
       </div>
       <div>duck dep dzai</div>
+      <div>{JSON.stringify(duckObject)}</div>
+      <div>{console.log("HELLO DUCK")}</div>
     </>
   );
 };
