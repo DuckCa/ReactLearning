3d31545fa4c181f0677323e4aaace17917c5128f
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-09-10 11:21:06 +0700

    video 36

--- src/components/learn/Input.jsx
+++ src/components/learn/Input.jsx
@@ -1,12 +1,18 @@
 const Input = (props) => {
   const { functionalert } = props;
-  functionalert("Duck");
+  // functionalert("Duck");
+  const eventhandle = (name) => {
+    console.log(name);
+  };
   return (
     <>
       <input
         className="input-text"
         type="text"
         placeholder="Enter your task"
+        onChange={(event) => {
+          eventhandle(event.target.value);
+        }}
       ></input>
       <button className="btn-add">ADD</button>
       <image src=""></image>
